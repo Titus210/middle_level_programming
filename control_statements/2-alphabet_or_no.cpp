712dc55dcfbdcef7c36a5ad94b68da7497281312
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-09-15 22:55:04 +0300

    alpha or not

--- control_statements/2-alphabet_or_no.cpp
+++ control_statements/2-alphabet_or_no.cpp
@@ -5,7 +5,7 @@ int main()
 	char  char1;
 	cout << "Enter a character \n";
 	cin char1;
-	(char1 >= 'a' && char1 <= 'z' || char1 >= 'A' && char1 <= 'Z') ? (cout << "%c is an alphabet " << char1) : ( cout << "%c is not an Alphabet ");
+	(char1 >= 'a' && char1 <= 'z' || char1 >= 'A' && char1 <= 'Z') ? (cout << char1 << " is an alphabet " ) : ( cout << char1 << " is not an Alphabet ");
 	return 0
 }
 
