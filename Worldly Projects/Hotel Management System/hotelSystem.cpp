408f5ee83a1c1db17a7057aa2974df7a8903cd0f
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-04-30 19:26:18 +0300

    Implemented Prices on each pizaa

--- Worldly Projects/Hotel Management System/hotelSystem.cpp
+++ Worldly Projects/Hotel Management System/hotelSystem.cpp
@@ -54,35 +54,30 @@ public:
 	double pizza_price = {14.00, 17.00, 12.00, 18.00, 16.00, 15.00};
 	double percentage_per_size = {0.0, 0.2, 0.35, 0.4} // percentage price for small, medium, large, xl
 	double price_per_size;
-	int choice_size, choice;
+	int choice_size, choice, price;
 	char pizza_type;
 
 	/* Display pizza  */
 	void pizzaMenu()
 	{
-		cout << "Choose Pizza size" << endl;
+		cout << "Choose pizza type" << endl;
 		for (int i = 0; i < pizza_type.size(); i++)
 		{
-			cout << i << ". "<< pizza_type[i] << endl;
+			cout << i + 1 << ". "<< pizza_type[i] << endl;
 		}
 		cin >> choice;
-		switch (choice)
-		{
-		case 0:
-			pizzaSize();
-			break;
-		case 1:
-			smallPrice();
-		case 2:
-			mediumPrice();
-		case 3:
-			largePrice();
-		case 4:
-			xLargePrice();
-		default:
-			cout << "Choose Right size" << endl;
-			pizzaSize();
-		}
+		/* get price depending on type */
+		if(choice == 1)
+			price = pizza_price[0];
+		else if(choice == 2)
+			price = pizza_price[1];
+		else if(choice == 3)
+			price  = pizza_price[2];
+		else if(choice == 4)
+			price = pizza_price[3];
+		else
+			price = price_pizza[4]
+		
 	}
 	/* method to display pizza size */
 	void pizzaSize()
@@ -100,20 +95,23 @@ public:
 		switch (choice)
 		{
 		case 1:
-			pizzaSize();
-			break;
-		case 2:
 			smallPrice();
-		case 3:
+		case 2:
 			mediumPrice();
-		case 4:
+		case 3:
 			largePrice();
-		case 5:
+		case 4:
 			xLargePrice();
 		default:
 			cout << "Choose Right size" << endl;
 			pizzaSize();
 		}
+	}
+
+	/* Small price pizzaa */
+
+	int smallPrice(){
+		return pizza_price
 	}
 
 
