6074b48619083055bc75e12aca0c1a2fd2f7b5ff
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-02 22:25:41 +0300

    Class Setup

---
+++ Worldly Projects/Hotel Management System/hotelManagement.cpp
@@ -0,0 +1,61 @@
+/**
+ * @file hotelManagement.cpp
+ * @author Titus Kiplagat
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/**
+ * Class Pizza
+ * Sub classes ["Pepperoni", "Veggie", "Chicago", "Detroit", "St. Louis", "Neapolitian"]
+ * @details Attributes
+ * name
+ * price
+ * size
+ * quantity
+ * availability
+ * price
+ * 
+ * @details Methods
+ * getPrice()
+ * getQuantity()
+ * getAvailability()
+ * getSize() 
+ */
+
+
+/**
+ * Class Sandwich
+ * Sub classes ["Club", "Chicken", "vegan", ]
+ * @details Attributes
+ * name
+ * size
+ * quantity
+ * availability
+ * price
+ 
+ * @details Methods
+ * getPrice()
+ * getQuantity()
+ * getAvailability()
+ */
+
+/**
+ * Class Burger
+ * Sub classes ["Chicken", "Beef",  ]
+ * @details Attributes
+ * name
+ * size
+ * quantity
+ * availability
+ * price
+
+ * @details Methods
+ * getPrice()
+ * getQuantity()
+ * getAvailability()
+ */
