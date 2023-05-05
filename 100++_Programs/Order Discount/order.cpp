1d2665a3ed3a97058d997cdaf9c4505b3f55c37b
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-05 15:21:28 +0300

    CONT: Updated price for each item getPrice() function to get price for  each item - main() dunction calls by reference with 2 memory location of Price adn no_of items The getPrice() function has no return values It directly updates the price variable

--- 100++_Programs/Order Discount/order.cpp
+++ 100++_Programs/Order Discount/order.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 
 int noOfItems();
+void getPrice( int *no_items, double *price);
+
 int main()
 {
+    int i;
+    double price = 0.0;
 
+    // prompt user to enter number of items to order
     int no_items = noOfItems();
     cout << no_items << endl;
+
+    // get price for all items
+    getPrice(&no_items, &price);
+    cout << price << endl;
+
+    return 0;
 }
 
 /* method to get items */
@@ -19,7 +30,17 @@ int noOfItems()
 }
 
 /* Method to get price of each Item */
-
+/* Method to get price of each Item */
+void getPrice(int *no_items, double *price)
+{
+    double item_price;
+    for (int i = 0; i < *no_items; i++)
+    {
+        cout << "Enter Price for Item " << i + 1 << endl;
+        cin >> item_price;
+        *price += item_price;
+    }
+}
 /* Calculate price of each item */
 
 /* Get discount for each item*/
